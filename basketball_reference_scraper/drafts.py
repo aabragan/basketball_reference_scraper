7210df85043a6dadd65a43058dcce0d23c92d1e0
Aniket Braganza <aabragan@gmail.com>
2024-09-15 14:55:49 -0400

    chore: formatting code with black & isort

--- basketball_reference_scraper/drafts.py
+++ basketball_reference_scraper/drafts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
+
 try:
     from request_utils import get_wrapper
 except:
@@ -7,28 +8,41 @@ except:
 
 
 def get_draft_class(year):
-    r = get_wrapper(f'https://www.basketball-reference.com/draft/NBA_{year}.html')
+    r = get_wrapper(f"https://www.basketball-reference.com/draft/NBA_{year}.html")
 
-
-    if r.status_code==200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        table = soup.find('table')
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
+        table = soup.find("table")
         df = pd.read_html(str(table))[0]
 
         # get rid of duplicate pick col
-        df.drop(['Unnamed: 0_level_0'], inplace=True, axis = 1, level=0)
-        df.rename(columns={'Unnamed: 1_level_0': '', 'Pk': 'PICK', 'Unnamed: 2_level_0': '', 'Tm': 'TEAM',
-                  'Unnamed: 5_level_0': '', 'Yrs': 'YEARS', 'Totals': 'TOTALS', 'Shooting': 'SHOOTING',
-                  'Per Game': 'PER_GAME', 'Advanced': 'ADVANCED', 'Round 1': '', 
-                  'Player': 'PLAYER', 'College': 'COLLEGE'}, inplace=True)
+        df.drop(["Unnamed: 0_level_0"], inplace=True, axis=1, level=0)
+        df.rename(
+            columns={
+                "Unnamed: 1_level_0": "",
+                "Pk": "PICK",
+                "Unnamed: 2_level_0": "",
+                "Tm": "TEAM",
+                "Unnamed: 5_level_0": "",
+                "Yrs": "YEARS",
+                "Totals": "TOTALS",
+                "Shooting": "SHOOTING",
+                "Per Game": "PER_GAME",
+                "Advanced": "ADVANCED",
+                "Round 1": "",
+                "Player": "PLAYER",
+                "College": "COLLEGE",
+            },
+            inplace=True,
+        )
 
         # flatten columns
-        df.columns = ['_'.join(x) if x[0] != '' else x[1] for x in df.columns]
+        df.columns = ["_".join(x) if x[0] != "" else x[1] for x in df.columns]
 
         # remove mid-table header rows
-        df = df[df['PLAYER'].notna()]
-        df = df[~df['PLAYER'].str.contains('Round|Player')]
+        df = df[df["PLAYER"].notna()]
+        df = df[~df["PLAYER"].str.contains("Round|Player")]
 
         return df
     else:
-        raise ConnectionError('Request to basketball reference failed')
+        raise ConnectionError("Request to basketball reference failed")
--- basketball_reference_scraper/request_utils.py
+++ basketball_reference_scraper/request_utils.py
@@ -1,15 +1,15 @@
-from requests import get
 from time import sleep, time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
+from requests import get
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 options = Options()
-options.add_argument('--headless=new')
+options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 last_request = time()
+
 
 def get_selenium_wrapper(url, xpath):
     global last_request
@@ -22,8 +22,9 @@ def get_selenium_wrapper(url, xpath):
         element = driver.find_element(By.XPATH, xpath)
         return f'<table>{element.get_attribute("innerHTML")}</table>'
     except:
-        print('Error obtaining data table.')
+        print("Error obtaining data table.")
         return None
+
 
 def get_wrapper(url):
     global last_request
@@ -37,7 +38,7 @@ def get_wrapper(url):
             return r
         elif r.status_code == 429:
             retry_time = int(r.headers["Retry-After"])
-            print(f'Retrying after {retry_time} sec...')
+            print(f"Retrying after {retry_time} sec...")
             sleep(retry_time)
         else:
             return r
